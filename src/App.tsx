1306ae2fe3fecfa480deb9b510f28186a71049b8
hieungocnguyen <ngnohieu@gmail.com>
2024-01-01 02:33:18 +0700

    Refactor class structure to improve readability and maintainability

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Editor from "./components/Editor";
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width:"100vw",
+        height:"100vh"
+      }}
+    >
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
